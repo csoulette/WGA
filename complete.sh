b342466d314993b27cad41020b93cf4c1b827ffb
Giridhar Nandigam <gnandiga@gmail.com>
2013-12-17 10:57:30 -0800

    My Latest

--- complete.sh
+++ complete.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-#promer MycoAviumk10.fasta query.fasta > mum_out
-show-coords out.delta > new.coords
+#promer MycoAviumk10.fasta query.fasta 
+show-coords -bHT out.delta > new.coords
+perl get_promum.pl new.coords > coords_by_org
 
+perl get_gaps.pl coords_by_org > gaps.fasta
+perl show-gaps2.pl gaps.fasta MycoTuberH37Rv.fasta MycoUlceransAgy99.fasta MycoAviumk10.fasta > complete.fasta
