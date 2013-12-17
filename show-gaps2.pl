94592c6b6074541f9bec8759edabfa0d6bea40c4
csoulette <csoulette@gmail.com>
2013-12-17 10:10:27 -0800

    message3

--- show-gaps2.pl
+++ show-gaps2.pl
@@ -1,41 +1,30 @@
 use English;
 
-$file1 = $ARGV[0];
-$file2 = $ARGV[1];
-$file3 = $ARGV[2];
+@fastas = @ARGV;
 
-open (IN, $file1);
-open (IN1,$file2);
-
-
-$/ = ">";
-
-<IN>; #Get rid of first line in $file1
-while (<IN>) {
+foreach $file (@fastas) {
+    ($file =~ /gaps/) 
+	&& ($gap_file = $file)
+	&& next;
+    
+    $/ = ">";
+    open (IN, $file);
+    <IN>; #Get rid of first line in $file
+    while (<IN>) {
     chomp;
     /\n/;
     ($header,$seq) = ($PREMATCH,$POSTMATCH);
     $seq =~ s/\n//g; #Make string on one line
     $sequence_hash{$header} = $seq;
+    }
 }
 
-<IN2>; #Get rid of first line in $file2
-while (<IN1>) {
-    chomp;
-    /\n/;
-    ($header,$seq) = ($PREMATCH,$POSTMATCH);
-    $seq =~ s/\n//g; 
-    $sequence_hash{$header} = $seq;
-}
-
-
-
-foreach $organism (keys %h) {
+foreach $organism (keys %sequence_hash) {
     length($organism) || next; #Gets rid of empty hash
     @sequence_array = $sequence_hash{$organism} =~ /(.)/g;
-    open (IN2, $file3); #This opens gaps.fasta
+    open (IN2, $gap_file); #This opens gaps.fasta
     $tally = 1; #Gap naming Convention
-
+    
     while (<IN2>) {
 	chomp;
 	/$organism/ || next; #Associates gap list with organism genome
